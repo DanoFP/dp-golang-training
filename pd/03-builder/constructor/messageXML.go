f42a60136cdb37da86a56f48b370807a1aed1ad3
unknown <dpineyro13@gmail.com>
2020-07-15 18:51:04 -0300

    lol

--- pd/03-builder/constructor/messageXML.go
+++ pd/03-builder/constructor/messageXML.go
@@ -7,24 +7,20 @@ import (
 	"github.com/DanoFP/dp-golang-training/pd/03-builder/message"
 )
 
-// XMLMessageBuilder is concrete builder
 type XMLMessageBuilder struct {
 	message message.Message
 }
 
-// SetRecipient asigna el receptor del mensaje
 func (b *XMLMessageBuilder) SetRecipient(recipient string) builder.MessageBuilder {
 	b.message.Recipient = recipient
 	return b
 }
 
-// SetMessage asigna el mensaje a enviar
 func (b *XMLMessageBuilder) SetMessage(text string) builder.MessageBuilder {
 	b.message.Text = text
 	return b
 }
 
-// Build construye el objeto y lo representa en XML
 func (b *XMLMessageBuilder) Build() (*message.MessageRepresented, error) {
 	data, err := xml.Marshal(b.message)
 	if err != nil {
